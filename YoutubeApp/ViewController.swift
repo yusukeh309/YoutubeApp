8d3cadb4c6bb6104140547723c7313716ed7d190
yusukeh309 <elmo.15vc@gmail.com>
2020-07-05 20:59:39 +0900

    YoutubeApp用のプロジェクトを作成

--- YoutubeApp/ViewController.swift
+++ YoutubeApp/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var videoListCollectionView: UICollectionView!
+    
+    private let cellId = "cellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        videoListCollectionView.backgroundColor = .red
+        videoListCollectionView.delegate = self
+        videoListCollectionView.dataSource = self
+        
+        videoListCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
         // Do any additional setup after loading the view.
     }
 
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = self.view.frame.width
+        
+        return .init(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = videoListCollectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+
+    
+}
